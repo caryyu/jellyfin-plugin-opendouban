79156a96a72b5ed5bc7cf16487ffbaf0c9bbf948
caryyu <caryyu.tg@gmail.com>
2021-06-25 18:05:15 +0800

    filter media name by regex

--- Jellyfin.Plugin.OpenDouban.Tests/MovieProviderTest.cs
+++ Jellyfin.Plugin.OpenDouban.Tests/MovieProviderTest.cs
@@ -8,6 +8,8 @@ using MediaBrowser.Model.Providers;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using Xunit.Abstractions;
+using System.Text.RegularExpressions;
+using Jellyfin.Plugin.OpenDouban.Configuration;
 
 namespace Jellyfin.Plugin.OpenDouban.Tests
 {
@@ -78,6 +80,37 @@ namespace Jellyfin.Plugin.OpenDouban.Tests
             };
             meta = _provider.GetMetadata(info, CancellationToken.None).Result;
             Assert.False(meta.HasMetadata);
+        }
+
+        [Fact]
+        public void TestNameRegexFiltering() {
+            PluginConfiguration cfg = new PluginConfiguration();
+
+            string[] names = {
+              "Loki.S01E03.HDR.2160p.WEB.H265-EXPLOIT.mkv",
+              "Minuscule.The.Valley.Of.The.Lost.Ants.2013.1080p.BluRay.H264.AAC-RARBG.mp4",
+              "Harry.Potter.and.the.Goblet.of.Fire.2005.1080p.BrRip.x264.YIFY.mp4",
+              "Harry.Potter.and.the.Sorcerers.Stone.2001.1080p.BrRip.x264.YIFY ( FIRST TRY).mp4",
+              "Harry.Potter.and.the.Sorcerer's.Stone.Extended.Cut.2001.720p.BrRip.x264.AAC.5.1.{MrMoviesFX}.【ThumperDC】.mkv",
+              "Ice Age Dawn of the Dinosaurs.2009.720p.BrRip.x264.YIFY.mp4",
+              "Titanic.1997.HDTV.1080p.x264.YIFY.mp4",
+              "The.Lion.King.2019.1080p.BluRay.x264-[YTS.LT].mp4",
+              "The.Croods.A.New.Age.2020.720p.WEBRip.800MB.x264-GalaxyRG.mkv"
+            };
+
+            // Loki
+            // Minuscule The Valley Of The Lost Ants 2013
+            // Harry Potter and the Goblet of Fire 2005
+            // Harry Potter and the Sorcerers Stone 2001
+            // Harry Potter and the Sorcerer's Stone Extended Cut 2001
+            // Ice Age Dawn of the Dinosaurs 2009
+            // Titanic 1997
+            // The Lion King 2019
+            // The Croods A New Age 2020
+
+            Assert.Equal("Loki", Regex.Replace(names[0], cfg.Pattern, " ").Trim());
+            Assert.Equal("The Lion King 2019", Regex.Replace(names[7], cfg.Pattern, " ").Trim());
+            Assert.Equal("The Croods A New Age 2020", Regex.Replace(names[8], cfg.Pattern, " ").Trim());
         }
     }
 }
--- Jellyfin.Plugin.OpenDouban/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OpenDouban/Configuration/PluginConfiguration.cs
@@ -10,8 +10,10 @@ namespace Jellyfin.Plugin.OpenDouban.Configuration
         {
             // MinRequestInternalMs = 2000;
             ApiBaseUri = "http://localhost:5000";
+            Pattern = @"(S\d{2}|E\d{2}|HDR|\d{3,4}p|WEBRip|WEB|YIFY|BrRip|BluRay|H265|H264|x264|AAC\.\d\.\d|AAC|HDTV|mkv|mp4)|(\[.*\])|(\-\w+|\{.*\}|【.*】|\(.*\)|\d+MB)|(\.|\-)";
         }
 
         public string ApiBaseUri  { get; set; }
+        public string Pattern { get; set; }
     }
 }
