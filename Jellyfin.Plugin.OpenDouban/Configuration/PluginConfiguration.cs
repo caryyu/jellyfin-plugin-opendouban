f2b4b06e1aebbdde3bee1541e68021a198d6477c
cxfksword <cxfksword@gmail.com>
2022-07-24 22:37:26 +0800

    Add poster quality config

--- Jellyfin.Plugin.OpenDouban/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OpenDouban/Configuration/PluginConfiguration.cs
@@ -11,9 +11,11 @@ namespace Jellyfin.Plugin.OpenDouban.Configuration
             // MinRequestInternalMs = 2000;
             ApiBaseUri = "http://localhost:5000";
             Pattern = @"(S\d{2}|E\d{2}|HDR|\d{3,4}p|WEBRip|WEB|YIFY|BrRip|BluRay|H265|H264|x264|AAC\.\d\.\d|AAC|HDTV|mkv|mp4)|(\[.*\])|(\-\w+|\{.*\}|【.*】|\(.*\)|\d+MB)|(\.|\-)";
+            PosterSize = "s";
         }
 
         public string ApiBaseUri { get; set; }
         public string Pattern { get; set; }
+        public string PosterSize { get; set; }
     }
 }
